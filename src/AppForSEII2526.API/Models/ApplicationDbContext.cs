958a20c77b90e39e0a6f18d92b88406a40f857cf
DAVID GÓMEZ ANIORTE <David.Gomez38@alu.uclm.es>
2025-09-28 16:24:48 +0200

    Cambios realizados en el archivo ApplicationDbContext.cs

---
+++ src/AppForSEII2526.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppForSEII2526.API.Models
+{
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
+    
+        }
+        protected override void OnModelCreating(ModelBuilder builder){
+           base.OnModelCreating(builder);
+        }
+    }
+}
+
+
