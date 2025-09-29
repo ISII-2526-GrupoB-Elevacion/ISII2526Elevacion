6a5758e9370cfee691e185feae6f3f0216c6f103
CARLOS DE LA GUÍA MARTÍNEZ-MECO <Carlos.Guia@alu.uclm.es>
2025-09-29 17:06:42 +0200

    Clase Purchase.cs creada

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,11 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        public string DeliveryCarDealer { get; set; }
+        public int Id { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime PurchasingDate { get; set; }
+        public float PurchasingPrice { get; set; }
+    }
+}
