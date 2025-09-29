7b737aa7e32f77eb3fba596e53d983e9318f54c2
Carlos de la Guía Martínez-Meco <Carlos.Guia@alu.uclm.es>
2025-09-29 16:57:50 +0200

    Clase Car.cs creada (#27)

---
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Car
+    {
+        public string CarClass { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public float PurchasingPrice { get; set; }
+        public int QuantityForPurchasing { get; set; }
+        public int QuantityForRenting { get; set; }
+        public int RentalItems { get; set; }
+        public float RentingPrice { get; set; }
+        public int ReviewItems { get; set; }
+    }
+}
