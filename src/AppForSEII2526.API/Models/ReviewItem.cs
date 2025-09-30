c74892678fd719a9cd50328affc6cadfb03be593
Francisco Díaz-Hellín Escribano <Fco.Diaz10@alu.uclm.es>
2025-09-30 17:23:47 +0200

    Creacion y modificacion de las clases del caso de uso de reseñar coches realizadas. (#42)

---
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -0,0 +1,10 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ReviewItem
+    {
+        public int CarId { get; set; }
+        public string Description { get; set; }
+        public float Rating { get; set; }
+        public int ReviewId { get; set; }
+    }
+}
