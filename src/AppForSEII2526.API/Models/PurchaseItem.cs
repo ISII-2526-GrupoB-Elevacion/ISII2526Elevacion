6807abf8bcb5461bc2a0844b63d1f0e90bdd47e5
Carlos de la Guía Martínez-Meco <Carlos.Guia@alu.uclm.es>
2025-10-04 11:32:39 +0200

    Creación de las referencias entre las clases Car, Purchase y PurchaseItem, además de la modificación del formato de las PKs de la clase PurchaseItem con respecto a la compra de coches (#70)

--- src/AppForSEII2526.API/Models/PurchaseItem.cs
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -1,13 +1,15 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(CarId), nameof(PurchaseId))]
     public class PurchaseItem
     {
-        [Key]
         public int CarId { get; set; }
-        [Key]
-        public int PurchaseId { get; set; }
+        public Car Car { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for Purchase is 1")]
         public int Quantity { get; set; }
+
+        public int PurchaseId { get; set; }
+        public Purchase Purchase { get; set; }
     }
 }
