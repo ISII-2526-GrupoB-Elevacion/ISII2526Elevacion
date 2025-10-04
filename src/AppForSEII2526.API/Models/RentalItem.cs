8f6cf5cf234577e0166f8bb1eb85b17cc0cf973c
David Gómez Aniorte <David.Gomez38@alu.uclm.es>
2025-10-04 09:50:20 +0200

    Se han modificado las clases Car, Model, Rental, y RentalItem para añadir las restricciones. Así como se ha añadidp el enum PaymentMethodEnum para añadir los tipos de pago (#63)

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -4,7 +4,10 @@
     {
         [Key]
         public int CarId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Minimum quantity for Renting is 1")]
         public int Quantity { get; set; }
+        
         [Key]
         public int RentalId { get; set; }
     }
