19dfe203adae5c5ed0c702c1fd6532c5a9acba29
Francisco Díaz-Hellín Escribano <Fco.Diaz10@alu.uclm.es>
2025-10-04 10:26:50 +0200

    Modificacion de las clases ReviewItem y Review para añadir las restricciones pertinentes a los atrubutos que las requieran. (#65)

--- src/AppForSEII2526.API/Models/ReviewItem.cs
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -4,8 +4,14 @@
     {
         [Key]
         public int CarId { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description cannot be any longer than 100 characters, neither shorter than 15.", MinimumLength = 15)]
         public string Description { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0, 10, ErrorMessage = "Minimum is 0 and maximum 10")]
         public float Rating { get; set; }
+
         [Key]
         public int ReviewId { get; set; }
     }
