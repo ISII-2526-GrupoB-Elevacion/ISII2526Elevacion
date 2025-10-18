4b45183060bed14e33119a7bc8435032628877f3
CARLOS DE LA GUÍA MARTÍNEZ-MECO <Carlos.Guia@alu.uclm.es>
2025-10-18 13:07:31 +0200

    Implementado método de get para ver los detalles con respecto a una compra de un coche

---
+++ src/AppForSEII2526.API/Controllers/PurchasesController.cs
@@ -0,0 +1,53 @@
+﻿using AppForSEII2526.API.DTOs.PurchaseDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchasesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<PurchasesController> _logger;
+
+        public PurchasesController(ApplicationDbContext context, ILogger<PurchasesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(PurchaseDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> Get_Details_Purchase_DTO(int id)
+        {
+            if (_context.Purchase == null)
+            {
+                _logger.LogError("Error: Purchase table does not exist");
+                return NotFound();
+            }
+
+            var purchase = await _context.Purchase
+             .Where(p => p.Id == id)
+                 .Include(p => p.ApplicationUser) //join table ApplicationUser
+                 .Include(p => p.PurchaseItems) //join table PurchaseItem
+                    .ThenInclude(pi => pi.Car) //then join table Car
+                        .ThenInclude(c => c.Model) //then join table Model     
+             .Select(p => new PurchaseDetailDTO(p.ApplicationUser.Name, p.ApplicationUser.Surname, p.PurchasingDate, p.PurchasingPrice, p.PurchaseItems
+                        .Select(pi => new PurchaseItemDTO(pi.Car.Model.Name, pi.Quantity, pi.Car.PurchasingPrice, pi.Car.Color)).ToList<PurchaseItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (purchase == null)
+            {
+                _logger.LogError($"Error: Rental with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(purchase);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTO/PurchaseItemDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTO
+{
+    public class PurchaseItemDTO
+    {
+        public string Model { get; set; }
+        public float PurchasingPrice { get; set; }
+        public string Color { get; set; }
+        public int Quantity { get; set; }
+
+        public PurchaseItemDTO(string model, int quantity, float purchasingPrice, string color)
+        {
+            Model = model;
+            Quantity = quantity;
+            PurchasingPrice = purchasingPrice;
+            Color = color;
+        }
+    }
+}
