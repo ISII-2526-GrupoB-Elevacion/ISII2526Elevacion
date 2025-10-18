3c861cb903ceadd77083bce7ee328e3cabd605f8
Fran <Fco.Diaz10@alu.uclm.es>
2025-10-18 10:55:16 +0200

    Se ha relizado el método get de la selección el reseñado de coches.

---
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -0,0 +1,36 @@
+﻿using AppForSEII2526.API.DTOs.CarDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForReviewDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetCars_ForReview_DTO(string? filtroManufacturer, string? filtroFuelType)
+        {
+            var car = await _context.Car
+                .Include(c => c.Model)
+                .Where(c => (c.Manufacturer.Contains(filtroManufacturer) || filtroManufacturer == null) && (c.FuelType.Contains(filtroFuelType) || (filtroFuelType == null)))
+                .Select(c => new CarForReviewDTO(c.Id, c.Model.Name, c.CarClass, c.Manufacturer, c.FuelType, c.Color)) 
+                .ToListAsync();
+            return Ok(car);
+        }
+
+    }
+}
