a2dce3062aedc9fd66830b336b25cb32d1b135e4
Carlos de la Guía Martínez-Meco <Carlos.Guia@alu.uclm.es>
2025-10-18 13:10:29 +0200

    Implementado método de get para ver los detalles con respecto a una compra de un coche (#110)

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTO/PurchaseDetailDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTO
+{
+    public class PurchaseDetailDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime PurchasingDate { get; set; }
+        public float PurchasingPrice { get; set; }
+        public IList<PurchaseItemDTO> PurchaseItems { get; set; }
+
+        public PurchaseDetailDTO(string name, string surname, DateTime purchasingDate, float purchasingPrice, IList<PurchaseItemDTO> purchaseItems)
+        {
+            Name = name;
+            Surname = surname;
+            PurchasingDate = purchasingDate;
+            PurchasingPrice = purchasingPrice;
+            PurchaseItems = purchaseItems;
+        }
+    }
+}
