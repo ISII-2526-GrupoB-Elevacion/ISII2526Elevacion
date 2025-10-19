18dc3a7d2cb600972ba3605612719868d95150a1
Fran <Fco.Diaz10@alu.uclm.es>
2025-10-19 09:47:22 +0200

    Implementado el método get para ver los detalles con respecto a la reseña de un coche.

---
+++ src/AppForSEII2526.API/Controllers/ReviewsController.cs
@@ -0,0 +1,53 @@
+﻿using AppForSEII2526.API.DTOs.ReviewDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ReviewsController> _logger;
+
+        public ReviewsController(ApplicationDbContext context, ILogger<ReviewsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReviewDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> Get_Details_Review_DTO(int id)
+        {
+            if (_context.Review == null)
+            {
+                _logger.LogError("Error: Review table does not exist");
+                return NotFound();
+            }
+
+            var review = await _context.Review
+             .Where(p => p.Id == id)
+                 .Include(p => p.ApplicationUser) //join table ApplicationUser
+                 .Include(p => p.ReviewItems) //join table ReviewItem
+                    .ThenInclude(pi => pi.Car) //then join table Car
+                        .ThenInclude(c => c.Model) //then join table Model     
+             .Select(p => new ReviewDetailDTO(p.ApplicationUser.Name, p.ApplicationUser.Surname, p.Country, p.DriverType, p.Created, p.ReviewItems
+                        .Select(pi => new ReviewItemDTO(pi.Car.Model.Name, pi.Car.Manufacturer, pi.Car.Color, pi.Rating, pi.Description)).ToList<ReviewItemDTO>()))
+             .FirstOrDefaultAsync();
+
+
+            if (review == null)
+            {
+                _logger.LogError($"Error: Review with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(review);
+        }
+    }
+}
