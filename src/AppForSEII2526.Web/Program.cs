dd7988720e98a28d3143181a7e85fa81f4f3bbb7
DAVID GÓMEZ ANIORTE <David.Gomez38@alu.uclm.es>
2025-11-25 17:43:50 +0100

    Se han elaborado los cambios necesarios para establecer la conexión desde la api con la web

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
+using AppForSEII2526.Web.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,6 +36,11 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+//this variable obtains the url where the API has been deployed
+string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526_API") as string;
+
+//We create the service for accessing the API from where .WEB project
+builder.Services.AddScoped<AppForSEII2526APIClient>(sp =>(AppForSEII2526APIClient)Activator.CreateInstance(typeof(AppForSEII2526APIClient), URI2API!, new HttpClient())!);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
