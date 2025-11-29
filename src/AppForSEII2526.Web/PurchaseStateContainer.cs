25f3364f1311c3dfc4aed5af8df6d8b3f2afb1c8
CARLOS DE LA GUÍA MARTÍNEZ-MECO <Carlos.Guia@alu.uclm.es>
2025-11-29 11:35:05 +0100

    Creado PurchaseStateContainer para la UI de "Comprar coches"

---
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -0,0 +1,65 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class PurchaseStateContainer
+    {
+        //creamos una instancia de Purchase cuando una instancia de PurchaseStateContainer es creada
+        public PurchaseForCreateDTO Purchase { get; private set; } = new PurchaseForCreateDTO()
+        {
+            PurchaseItems = new List<PurchaseItemDTO>()
+        };
+
+        //calculamos el precio total de los coches que queremos comprar
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Convert.ToDecimal(Purchase.PurchaseItems.Sum(pi => pi.PurchasingPrice));
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+        public void AddCarToPurchase(CarForPurchaseDTO car)
+        {
+            //before adding a car we checked whether it has been already added
+            if (!Purchase.PurchaseItems.Any(pi => pi.Model == car.Model))
+                //we add it if it is not in the list
+                Purchase.PurchaseItems.Add(new PurchaseItemDTO()
+                {
+                    Model = car.Model,
+                    PurchasingPrice = (float)car.PurchasingPrice,
+                    Color = car.Color
+                }
+            );
+        }
+
+        //to delete movies from the list of selected movies
+        public void RemoveRentalItemToRent(PurchaseItemDTO item)
+        {
+            Purchase.PurchaseItems.Remove(item);
+
+        }
+
+        //we eliminate all the movies from the list
+        public void ClearRentingCart()
+        {
+            Purchase.PurchaseItems.Clear();
+
+        }
+
+        //we have already finished the process of renting, thus, we create a new Rental 
+        public void RentalProcessed()
+        {
+            //we have finished the rental process so we create a new object without data
+            Purchase = new PurchaseForCreateDTO()
+            {
+                PurchaseItems = new List<PurchaseItemDTO>()
+            };
+        }
+    }
+}
