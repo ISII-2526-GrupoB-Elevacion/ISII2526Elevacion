951bf33ff4208cc1229d8333c6256c4d86a374d6
Carlos de la Guía Martínez-Meco <Carlos.Guia@alu.uclm.es>
2025-12-01 18:34:39 +0100

    Realizada la interfaz de usuario para la selección de coches para comprar, además de agregar el servicio del state container al program.cs, debido a que faltaba de un commit anterior. De forma adicional, se han cambiado algunos nombres de métodos y comentarios del contenedor de estado de "Purchase" (#196)

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+builder.Services.AddScoped<PurchaseStateContainer>();
 
 builder.Services.AddAuthentication(options =>
     {
