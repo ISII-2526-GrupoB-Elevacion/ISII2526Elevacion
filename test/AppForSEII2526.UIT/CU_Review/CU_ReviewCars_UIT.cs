56e636b20d6fccff5fa0c479a3e75eb83cbea041
Fran <Fco.Diaz10@alu.uclm.es>
2025-12-13 12:08:11 +0100

    Se suben los cambios referentes a las pruebas del select del caso de prueba de reseñar coches

---
+++ test/AppForSEII2526.UIT/CU_Review/CU_ReviewCars_UIT.cs
@@ -0,0 +1,100 @@
+﻿using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.CU_Review;
+using AppForSEII2526.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Review
+{
+    public class CU_ReviewCars_UIT : UC_UIT{
+
+        private SelectCarsForReview_PO selectCarsForReview_PO;
+        private const int carId1 = 1;
+        private const string carModel1 = "Audi A4";
+        private const string carCarClass1 = "Berlina";
+        private const string carManufacturer1 = "Audi";
+        private const string carFuelype1 = "Gasolina";
+        private const string carColor1 = "Gris";
+
+        private const string carModel2 = "Toyota Corolla";
+        private const string carCarClass2 = "Familiar";
+        private const string carManufacturer2 = "Toyota";
+        private const string carFuelype2 = "Diesel";
+        private const string carColor2 = "Rojo";
+
+        public CU_ReviewCars_UIT(ITestOutputHelper output) : base(output) {
+
+            selectCarsForReview_PO = new SelectCarsForReview_PO (_driver, _output);
+        }
+            
+        private void Precondition_perform_login() {
+            Perform_login("elena@uclm.es", "Password1234%");
+        }
+
+        private void InitialStepsForReviewCars()
+        {
+            Precondition_perform_login();
+            var byCreate = By.Id("CreateReview");
+            const int maxAttempts = 3;
+            for (int attempt = 1; attempt <= maxAttempts; attempt++)
+            {
+                try
+                {
+                    selectCarsForReview_PO.WaitForBeingClickable(byCreate);
+                    _driver.FindElement(byCreate).Click();
+                    break;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    if (attempt == maxAttempts) throw;
+                    Thread.Sleep(250);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(carModel1, carCarClass1, carManufacturer1, carFuelype1,carColor1, "Audi", "")]
+        [InlineData(carModel2, carCarClass2, carManufacturer2, carFuelype2, carColor2, "", "Diesel")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC4_AF0_UC4_2_3_filtering(string carModel, string carCarClass,string carManufacturer, string carFuelype, string carColor, string filterManufacturer, string filterFuelType)
+        {
+            //Arrange
+            InitialStepsForReviewCars();
+            var expectedCars = new List<string[]> { new string[] {carModel,carCarClass,carManufacturer,carFuelype,carColor }, };
+
+            //Act
+            selectCarsForReview_PO.SearchCars(filterManufacturer, filterFuelType);
+
+            //Assert
+
+            Assert.True(selectCarsForReview_PO.CheckListOfCars(expectedCars));
+
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+
+        public void UC4_AF1_UC4_4_ReviewNotavailable()
+        {
+            //Arrange
+            InitialStepsForReviewCars();
+            //Act
+            selectCarsForReview_PO.AddCarToReviewCart(carModel1);
+            selectCarsForReview_PO.RemoveCarFromReviewCart(carModel1);
+
+            //Assert
+
+            Assert.True(selectCarsForReview_PO.ReviewNotAvailable());
+
+        }
+
+    }
+
+}
+
+    
+    
---
+++ test/AppForSEII2526.UIT/CU_Review/SelectCarsForReview_PO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.CU_Review
+{
+    public class SelectCarsForReview_PO : PageObject
+    {
+        By inputManufacturer = By.Id("inputManufacturer");
+        By inputFuelType = By.Id("inputFuelType");
+        By buttonSearchCars = By.Id("searchCars");
+        By tableOfCarsBy = By.Id("TableOfCars");
+        By errorShownBy = By.Id("ErrorsShown");
+        By buttonReviewCars = By.Id("reviewCarButton");
+
+        public SelectCarsForReview_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchCars(string manufacturer, string fueltype)
+        {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputManufacturer);
+            _driver.FindElement(inputManufacturer).SendKeys(manufacturer);
+            if (fueltype == "") fueltype = "All";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(inputFuelType));
+            selectElement.SelectByText(fueltype);
+
+            _driver.FindElement(buttonSearchCars).Click();
+
+
+        }
+
+        public bool CheckListOfCars(List<string[]> expectedCars)
+        {
+
+            return CheckBodyTable(expectedCars, tableOfCarsBy);
+        }
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual Message shown:{actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+        public void AddCarToReviewCart(string carModel)
+        {
+            WaitForBeingClickable(By.Id("carToReview_" + carModel));
+
+            _driver.FindElement(By.Id("carToReview_" + carModel)).Click();
+        }
+
+        public void RemoveCarFromReviewCart(string  carModel)
+        {
+            WaitForBeingClickable(By.Id("removeCar_" + carModel));
+            _driver.FindElement(By.Id("removeCar_" + carModel)).Click();
+        }
+
+        public bool ReviewNotAvailable()
+        {
+            //the button is not Displayed=hidden
+
+            return _driver.FindElement(buttonReviewCars).Displayed == false;
+        }
+    }
+}
