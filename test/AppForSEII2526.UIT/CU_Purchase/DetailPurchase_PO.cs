874d6063d25b8a4468aa36f6986d8851b573633d
Carlos de la Guía Martínez-Meco <Carlos.Guia@alu.uclm.es>
2025-12-14 12:52:25 +0100

    Implementadas las pruebas funcionales para la IU del details del caso de uso "Purchase Cars". (#264)

---
+++ test/AppForSEII2526.UIT/CU_Purchase/DetailPurchase_PO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Purchase
+{
+    public class DetailPurchase_PO : PageObject
+    {
+        public DetailPurchase_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+
+        }
+
+        public bool CheckPurchaseDetail(string nameSurname, string deliveryCarDealer, DateTime purchasingDate, string totalPrice)
+        {
+            Thread.Sleep(500);
+            WaitForBeingVisible(By.Id("TotalPrice"));
+            bool result = true;
+            result = result && _driver.FindElement(By.Id("NameSurname")).Text.Contains(nameSurname);
+            result = result && _driver.FindElement(By.Id("DeliveryCarDealer")).Text.Contains(deliveryCarDealer);
+            result = result && _driver.FindElement(By.Id("TotalPrice")).Text.Contains(totalPrice);
+
+            var actualPurchaseDate = DateTime.Parse(_driver.FindElement(By.Id("PurchasingDate")).Text);
+            result = result && ((actualPurchaseDate - purchasingDate) < new TimeSpan(0, 1, 0));
+
+            return result;
+        }
+
+        public bool CheckListOfPurchase(List<string[]> expectedRentalItems)
+        {
+            return CheckBodyTable(expectedRentalItems, By.Id("PurchasedCars"));
+        }
+    }
+}
