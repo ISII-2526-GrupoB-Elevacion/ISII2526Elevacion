6f9fbf1e47975720011e9fa2d40f0934bfc7df71
David Gómez Aniorte <David.Gomez38@alu.uclm.es>
2025-12-14 18:32:40 +0100

    Se han implementado las pruebas de la interfaz del post con respecto al alquiler de coches (#265)

--- test/AppForSEII2526.UIT/CU-Rental/CU_RentalCars_UIT.cs
+++ test/AppForSEII2526.UIT/CU-Rental/CU_RentalCars_UIT.cs
@@ -1,4 +1,6 @@
-﻿using AppForSEII2526.UIT.Shared;
+﻿using AppForSEII2526.UIT.CU_Purchase;
+using AppForSEII2526.UIT.CU_Rental;
+using AppForSEII2526.UIT.Shared;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -12,6 +14,13 @@ namespace AppForSEII2526.UIT.CU_RentalCars
     public class CU_RentalCars_UIT: UC_UIT
     {
         private SelectCarsForRental_PO selectCarsForRental_PO;
+        private CreateRental_PO createRental_PO;
+
+        private const string Name = "Elena";
+        private const string SurName = "Navarro Martínez";
+        private const string DeliveryCarDealer = "Calle Albacete";
+        private const string PaymentMethod = "Visa";
+        private const string Quantity= "1";
 
         private const int carId1 = 1;
         private const string CarModel1= "Audi A4";
@@ -26,9 +35,11 @@ namespace AppForSEII2526.UIT.CU_RentalCars
         private const string Manufacturer2 = "Toyota";
         private const string RentingPrice2 = "60";
         private const string Color2 = "Rojo";
+
         public CU_RentalCars_UIT(ITestOutputHelper output): base(output)
         {
             selectCarsForRental_PO = new SelectCarsForRental_PO(_driver, _output);
+            createRental_PO = new CreateRental_PO(_driver, _output);
         }
         private void Precondition_perform_login()
         {
@@ -82,8 +93,7 @@ namespace AppForSEII2526.UIT.CU_RentalCars
 
         [Fact]
         [Trait("LevelTesting", "Funcional Testing")]
-
-        public void CU2_AF2_UC2_6_RentingNotavailable()
+        public void CU2_FA2_CU2_6_RentingNotavailable()
         {
             //Arrange
             InitialStepsForRentalCars();
@@ -95,6 +105,110 @@ namespace AppForSEII2526.UIT.CU_RentalCars
 
             Assert.True(selectCarsForRental_PO.RentingNotAvailable());
 
+        }
+        public static IEnumerable<object[]> TestCasesFor_CU2_9_10_FA4_errorindates()
+        {
+            var allTests = new List<object[]> {
+                new object[] {DateTime.Today.AddDays(-1), DateTime.Today.AddDays(2), DateTime.Today.AddDays(1), "Your rental period must be later",  },
+                //cannot be checked if datetime is before today, because the next condition is checked before
+                new object[] {DateTime.Today.AddDays(2), DateTime.Today.AddDays(1), DateTime.Today.AddDays(1), "Your rental must end after than its starts", },
+            };
+
+            return allTests;
+        }
+
+        public void CU2_FA3_CU1_7_DeleteSelectedCars()
+        {
+            //Arrange
+            InitialStepsForRentalCars();
+
+            //Act
+            selectCarsForRental_PO.AddCarToRentingCart(CarModel1);
+            selectCarsForRental_PO.AddCarToRentingCart(CarModel2);
+            selectCarsForRental_PO.RentCars();
+
+            createRental_PO.FillInPurchaseQuantity(Quantity, CarModel1);
+            createRental_PO.FillInPurchaseQuantity(Quantity, CarModel2);
+            createRental_PO.PressModifyCars();
+
+            selectCarsForRental_PO.RemoveCarFromRentingCart(CarModel2);
+            selectCarsForRental_PO.RentCars();
+
+            //Assert
+            var expectedTotalPrice = "85";
+            Assert.True(createRental_PO.CheckTotalPrice(expectedTotalPrice));
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCasesFor_CU2_9_10_FA4_errorindates))]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CU2_8_9_FA2_errorindates(DateTime from, DateTime to, DateTime renting, string expectedMessageError)
+        {
+            //Arrange
+
+
+            //Act
+            InitialStepsForRentalCars();
+
+            selectCarsForRental_PO.SearchCars("", null, from.ToString("dd-MM-yyyy"), to.ToString("dd-MM-yyyy"), renting.ToString("dd-MM-yyyy"));
+            Thread.Sleep(1000);
+            //Assert
+
+            //this message will be shown if assert fails
+            Assert.True(selectCarsForRental_PO.CheckMessageError(expectedMessageError), $"Expected error: {expectedMessageError}");
+        }
+
+        [Theory]
+        [InlineData("",SurName,DeliveryCarDealer,PaymentMethod,"The field Name must be a string with a minimum length of 2 and a maximum length of 20.")]
+        [InlineData(Name, "", DeliveryCarDealer, PaymentMethod, "The field Surname must be a string with a minimum length of 4 and a maximum length of 100.")]
+        [InlineData(Name, SurName, "", PaymentMethod, "The field DeliveryCarDealer must be a string with a minimum length of 1 and a maximum length of 20.")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CU2_10_11_12_FA4_testingErrorsMandatorydata(string name, string surname, string deliveryCarDealer,string paymentMethod,string expectedMessageError)
+        {
+            //Arrange
+
+            var from = DateTime.Today.AddDays(1);
+            var to = DateTime.Today.AddDays(2);
+            var renting = DateTime.Today.AddDays(1);
+            //Act
+            InitialStepsForRentalCars();
+
+            selectCarsForRental_PO.SearchCars("",null,from.ToString("dd-MM-yyyy"), to.ToString("dd-MM-yyyy"), renting.ToString("dd-MM-yyyy"));
+            selectCarsForRental_PO.AddCarToRentingCart(CarModel1);
+            selectCarsForRental_PO.RentCars();
+            createRental_PO.FillInRentalInfo(name,surname,deliveryCarDealer,paymentMethod);
+            createRental_PO.PressRentYourCars();
+
+            //Assert
+            //the expected error is shown in the view
+            Assert.True(createRental_PO.CheckValidationError(expectedMessageError), $"Expected error: {expectedMessageError}");
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CU2_13_FA5_ModifyRentalItems()
+        {
+            //Arrange
+
+            var from = DateTime.Today.AddDays(1);
+            var to = DateTime.Today.AddDays(2);
+            var renting = DateTime.Today.AddDays(1);
+            //Act
+            InitialStepsForRentalCars();
+
+            selectCarsForRental_PO.SearchCars("", null, from.ToString("dd-MM-yyyy"), to.ToString("dd-MM-yyyy"), renting.ToString("dd-MM-yyyy"));
+            selectCarsForRental_PO.AddCarToRentingCart(CarModel1);
+            selectCarsForRental_PO.AddCarToRentingCart(CarModel2);
+            selectCarsForRental_PO.RentCars();
+            createRental_PO.PressModifyCars();
+            //we remove movietitle2 from the rentingcart
+            selectCarsForRental_PO.RemoveCarFromRentingCart(CarModel1);
+            selectCarsForRental_PO.RentCars();
+
+            //Assert
+            //the list of cars must change
+            var expectedRentalItems = new List<string[]> { new string[] { CarModel2, Manufacturer2, RentingPrice2 }, };
+            Assert.True(createRental_PO.CheckListOfRentalItems(expectedRentalItems));
         }
     }
 }
---
+++ test/AppForSEII2526.UIT/CU-Rental/CreateRental_PO.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_Rental
+{
+    internal class CreateRental_PO: PageObject
+    {
+        private By Name = By.Id("RentalName");
+        private By Surname = By.Id("RentalSurname");
+        private By DeliveryCarDealer = By.Id("RentalDeliveryCarDealer");
+        private By PaymenMethod = By.Id("RentalPaymentMethod");
+        private By TotalPrice = By.Id("RentalTotalPrice");
+
+        private IWebElement _name() => _driver.FindElement(Name);
+        private IWebElement _surname() => _driver.FindElement(Surname);
+        private IWebElement _deliveryCarDealer() => _driver.FindElement(DeliveryCarDealer);
+        private IWebElement _paymentMethod() => _driver.FindElement(PaymenMethod);
+        public CreateRental_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+
+        }
+
+        public void FillInPurchaseQuantity(string quantity, string model)
+        {
+            WaitForBeingClickable(By.Id("quantity_" + model));
+            _driver.FindElement(By.Id("quantity_" + model)).Clear();
+            _driver.FindElement(By.Id("quantity_" + model)).SendKeys(quantity);
+        }
+
+        public void FillInRentalInfo(string name, string surname, string deliveryCarDealer, string paymentMethod)
+        {
+            WaitForBeingVisible(Name);
+            WaitForBeingVisible(Surname);
+            WaitForBeingVisible(DeliveryCarDealer);
+            _name().SendKeys(name);
+            _surname().SendKeys(surname);
+            _deliveryCarDealer().SendKeys(deliveryCarDealer);
+
+            //create select element object 
+            SelectElement selectElement = new SelectElement(_paymentMethod());
+
+            //select Action from the dropdown menu
+            selectElement.SelectByText(paymentMethod);
+        }
+
+        public void PressRentYourCars()
+        {
+            _driver.FindElement(By.Id("RentalSubmit")).Click();
+        }
+        public void PressModifyCars()
+        {
+            _driver.FindElement(By.Id("ModifyRental")).Click();
+        }
+
+        public bool CheckListOfRentalItems(List<string[]> expectedRentalItems)
+        {
+            return CheckBodyTable(expectedRentalItems, By.Id("TableOfRentalItems"));
+        }
+
+        public bool CheckTotalPrice(string expectedTotalPrice)
+        {
+            return _driver.FindElement(TotalPrice).GetAttribute("value").Equals(expectedTotalPrice);
+        }
+
+        public bool CheckValidationError(string expectedError)
+        {
+            return _driver.PageSource.Contains(expectedError);
+        }
+    }
+}
